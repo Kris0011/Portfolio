e738b511ddeaf79113e00b3c0ce87bdc4e4d633b
Kris0011 <krishp759@gmail.com>
2023-10-17 21:30:14 +0530

    Project and contact page components added

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,33 @@
+"use client";
 import { title } from "@/components/primitives";
+import { Input, Textarea, Button } from "@nextui-org/react";
 
 export default function PricingPage() {
-	return (
-		<div>
-			<h1 className={title()}>Contact</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1 className={title()}>Contact Me</h1>
+      <div className="mt-10 space-x-2">
+        <div className="space-y-2 mt-1 ">
+          <Input size="md" type="text" label="Name" variant="underlined" />
+          <Input size="md" type="email" label="Email" variant="underlined" />
+          <Input size="md" type="text" label="Subject" variant="underlined" />
+          <Textarea
+            key="flat"
+            variant="flat"
+            minRows={5}
+            labelPlacement="outside"
+            placeholder="Enter your message"
+            className="col-span-12 md:col-span-6 mb-6 md:mb-0 "
+          />
+
+          <Button
+            radius="full"
+            className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+          >
+            Submit
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 import { title } from "@/components/primitives";
 import ProjectCard from "@/components/ProjectCard";
-import { Accordion, AccordionItem } from "@nextui-org/react";
+import { Accordion, AccordionItem, Button } from "@nextui-org/react";
+
 import Image from "next/image";
 import connect from "../../public/connect.png";
+import news88 from "../../public/news88.png";
+import namegenius from "../../public/namegenius.png";
 
 export default function BlogPage() {
-  const temp =
-    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A similique magni vitae beatae dolorem quo rerum aperiam. Dicta, ex error!";
-
   return (
     <div>
       <Accordion variant="splitted" className="mt-10">
         <AccordionItem
-		  className="dark"
+          className="dark"
           key="1"
           aria-label="Accordion 1"
           title={
@@ -24,39 +24,76 @@ export default function BlogPage() {
                 src={connect}
                 width={800}
               />
-              <p className="mt-5 ">Connect - Fresh Start Guide (MERN Full Stack)</p>
+              <p className="mt-5 ">
+                Connect - Fresh Start Guide (MERN Full Stack)
+              </p>
             </div>
           }
         >
-			<div className="text-left p-3 text-white">
-				<p>Students can connect through chat, audio, and video calls for real-time interaction.</p>
-				<p>Enables seeking help, discussing topics, and effective collaboration.</p>
-				<p> Features like Notes Sharing and Blogging .</p>
-				<a href="https://github.com/its-mahi/connect_FreshStartGuide/tree/main" target="_blank" className="text-blue-600">Github Link</a>
-			</div>
+          <div className="text-left p-3 text-white">
+            <p>
+              Students can connect through chat, audio, and video calls for
+              real-time interaction.
+            </p>
+            <p>
+              Enables seeking help, discussing topics, and effective
+              collaboration.
+            </p>
+            <p> Features like Notes Sharing and Blogging .</p>
+            <a
+              href="https://github.com/its-mahi/connect_FreshStartGuide/tree/main"
+              target="_blank"
+              className="text-blue-600"
+            >
+              <Button color="primary" variant="ghost" size="sm" className="mt-3">
+                Github
+              </Button>
+            </a>
+          </div>
         </AccordionItem>
 
-
-
-        <AccordionItem  className="dark" key="2" aria-label="Accordion 2" title={
+        <AccordionItem
+          className="dark"
+          key="2"
+          aria-label="Accordion 2"
+          title={
             <div className="text-center">
               <Image
                 alt="Album cover"
                 className="object-cover"
-                src={connect}
+                src={news88}
                 width={800}
               />
               <p className="mt-5">News88 - The News App (Reactjs)</p>
             </div>
-          }>
-			<div className="text-left p-3 text-white">
-				<p>Simple React-WebApp to show to the current news from different categories.</p>
-				<p>React concepts like Router-DOM , Infinte Scroll , Top-Loading-bar etc.</p>
-				<a href="https://github.com/its-mahi/connect_FreshStartGuide/tree/main" target="_blank" className="text-blue-600">Github Link</a>
-			</div>
-		   
+          }
+        >
+          <div className="text-left p-3 text-white">
+            <p>
+              Simple React-WebApp to show to the current news from different
+              categories.
+            </p>
+            <p>
+              React concepts like Router-DOM , Infinte Scroll , Top-Loading-bar
+              etc.
+            </p>
+            <a
+              href="https://github.com/Kris0011/LearnReact/tree/main/newsapp"
+              target="_blank"
+              className="text-blue-600"
+            >
+              <Button color="primary" variant="ghost" size="sm" className="mt-3">
+                Github
+              </Button>
+            </a>
+          </div>
         </AccordionItem>
-        <AccordionItem className="dark" key="3" aria-label="Accordion 3" title={
+
+        <AccordionItem
+          className="dark"
+          key="3"
+          aria-label="Accordion 3"
+          title={
             <div className="text-center">
               <Image
                 alt="Album cover"
@@ -66,15 +103,67 @@ export default function BlogPage() {
               />
               <p className="mt-5">Attendence System (PHP + MySQL)</p>
             </div>
-          }>
-			<div className="text-left p-3 text-white">
-				<p>Login system for Admin , Teachers and students . Admin can add teachers and students . Students can login anytime anywhere and check their attedence graph . Teachers have features to add lectures and taking attedence.</p>
-				<a href="https://github.com/its-mahi/connect_FreshStartGuide/tree/main" target="_blank" className="text-blue-600">Github Link</a>
-			</div>
-		   
+          }
+        >
+          <div className="text-left p-3 text-white">
+            <p>
+              Login system for Admin , Teachers and students . Admin can add
+              teachers and students . Students can login anytime anywhere and
+              check their attedence graph . Teachers have features to add
+              lectures and taking attedence.
+            </p>
+            <a
+              href="https://github.com/Kris0011/Attendence_System"
+              target="_blank"
+              className="text-blue-600"
+            >
+              <Button color="primary" variant="ghost" size="sm" className="mt-3">
+                Github
+              </Button>
+            </a>
+          </div>
         </AccordionItem>
 
-        
+        <AccordionItem
+          className="dark"
+          key="4"
+          aria-label="Accordion 4"
+          title={
+            <div className="text-center">
+              <Image
+                alt="Album cover"
+                className="object-cover"
+                src={namegenius}
+                width={800}
+              />
+              <p className="mt-5">NameGenius (Reactjs)</p>
+            </div>
+          }
+        >
+          <div className="text-left p-3 text-white">
+            <p>Predict the Age , Gender and Country based on name .</p>
+            <div className="mt-3 space-x-2">
+              <a
+                href="https://github.com/Kris0011/LearnReact/tree/main/namegenius"
+                target="_blank"
+                className="text-blue-600"
+              >
+                <Button color="primary" variant="ghost" size="sm">
+                  Github
+                </Button>
+              </a>
+              <a
+                href="https://namegenius.netlify.app/"
+                target="_blank"
+                className="text-blue-600"
+              >
+                <Button color="primary" variant="ghost" size="sm">
+                  Demo
+                </Button>
+              </a>
+            </div>
+          </div>
+        </AccordionItem>
       </Accordion>
     </div>
   );
