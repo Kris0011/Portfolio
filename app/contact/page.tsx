35811cfdd2b3bf195141b7d23b2eee9ea25bde0b
Kris0011 <krishp759@gmail.com>
2023-10-18 18:16:22 +0530

    MongoDB local connection for contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,16 +1,34 @@
 "use client";
+import { useState } from "react";
 import { title } from "@/components/primitives";
 import { Input, Textarea, Button } from "@nextui-org/react";
+import axios from "axios";
 
 export default function PricingPage() {
+  const [name , setName] = useState('');
+  const [email , setEmail] = useState('');
+  const [subject , setSubject] = useState('');
+  const [message , setMessage] = useState('');
+
+  const postContactData = async () => {
+    const data = {name,email,subject,message};
+    console.log(data);
+    try {
+      const response = await axios.post('http://localhost:5000/api/contact', data);
+      console.log('Data has been successfully saved.', response.data);
+    } catch (error) {
+      console.error('Error saving data.', error);
+    }
+  };
+
   return (
     <div>
       <h1 className={title()}>Contact Me</h1>
       <div className="mt-10 space-x-2">
         <div className="space-y-2 mt-1 ">
-          <Input size="md" type="text" label="Name" variant="underlined" />
-          <Input size="md" type="email" label="Email" variant="underlined" />
-          <Input size="md" type="text" label="Subject" variant="underlined" />
+          <Input size="md" type="text" label="Name" variant="underlined" value={name} onChange={(e)=> setName(e.target.value)}/>
+          <Input size="md" type="email" label="Email" variant="underlined" value={email} onChange={(e)=> setEmail(e.target.value)}/>
+          <Input size="md" type="text" label="Subject" variant="underlined" value={subject} onChange={(e)=> setSubject(e.target.value)}/>
           <Textarea
             key="flat"
             variant="flat"
@@ -18,11 +36,13 @@ export default function PricingPage() {
             labelPlacement="outside"
             placeholder="Enter your message"
             className="col-span-12 md:col-span-6 mb-6 md:mb-0 "
+            value={message} onChange={(e)=> setMessage(e.target.value)} 
           />
 
           <Button
             radius="full"
             className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"
+            onClick={postContactData}
           >
             Submit
           </Button>
