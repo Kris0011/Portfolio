c11af838575c23a8db5fdeb28bc2ad2996a2d44a
Kris0011 <krishp759@gmail.com>
2024-11-30 00:01:15 +0530

    made some minor changes for hero and projects

--- components/hero.tsx
+++ components/hero.tsx
@@ -21,25 +21,31 @@ const projects: Project[] = [
     href: "https://github.com/Kris0011/CricketAdda",
   },
   {
-    title: "AgroZenith (Built at Hackout 2024 hackathon)",
+    title: "AgroZenith (Hackout 2024)",
     description:
       "An online auction platform for farmers to list and sell produce, with crop-specific fertilizer recommendations and a real-time fire alert system. Built with React, Flask, Python, Express.js, Node.js, and MongoDB.",
     href: "https://github.com/Kris0011/Hackout2024",
   },
   {
-    title: "GyaanGanga (Built at AceHack 3.0)",
+    title: "GyaanGanga (AceHack 3.0)",
     description:
       "A platform that provides real-time lecture streaming with automatic captions, transforming captions into organized notes to enhance learning.",
     href: "https://devfolio.co/projects/gyaanganga-6157",
   },
+  // {
+  //   title: "KisaanSathi (DotSlash 7.0)",
+  //   description:
+  //     "A WebSocket-based auction system for farmers and traders to engage in live bidding. Built with WebSockets and Node.js.",
+  //   href: "https://github.com/Kris0011/KisaanSathi",
+  // },
   {
-    title: "Connect - Fresh Start Guide (Built at TicTechToe'23)",
+    title: "Connect - Fresh Start Guide (TicTechToe'23)",
     description:
       "Fostering student connections and collaborative learning. Built at TicTechToe'23 hackathon.",
     href: "https://github.com/Kris0011/Portfolio",
   },
   {
-    title: "qUIck-react (Part of Hacktoberfest 2023)",
+    title: "qUIck-react (Hacktoberfest 2023)",
     description:
       "An open source UI library for React. Part of Hacktoberfest 2023.",
     href: "https://github.com/developer-student-clubs/qUIck-react",
@@ -56,39 +62,46 @@ const Hero = () => {
   return (
     <section id="home">
       {/* <Reveal> */}
-      <div className="font-poppins">
-        <div className="w-full  flex justify-center flex-col-reverse lg:flex-row items-center">
-          <Image
-            src="/krispatel.jpeg"
-            alt="Kris Patel"
-            width={200}
-            height={150}
-            className="rounded-[30%] m-4 shadow-md border border-gray-700"
-          />
+      <div className="font-poppins flex md:flex-row flex-col  justify-evenly items-center  md:space-x-2">
+        <div>
+          <div className="w-full  flex justify-center flex-col-reverse lg:flex-row items-center">
+            <Image
+              src="/krispatel.jpeg"
+              alt="Kris Patel"
+              width={180}
+              height={130}
+              className="rounded-[30%] mt-4 my-2 shadow-md border border-gray-700"
+            />
+          </div>
+          <div className="space-y-1">
+            <h1 className="w-full flex justify-center items-center flex-col-reverse text-black dark:text-white text-xl sm:text-2xl">
+              {/* <span className="opacity-60 text-sm">Student & Developer</span> */}
+              <b>Kris Patel</b>
+            </h1>
+          </div>
         </div>
-        <div className="space-y-6">
-          <h1 className="w-full flex justify-center items-center flex-col-reverse text-black dark:text-white text-xl sm:text-2xl">
-            <span className="opacity-60 text-lg">Student & Developer</span>
-            <b>Kris Patel</b>
-          </h1>
-        </div>
-      </div>
-      {/* </Reveal> */}
-      <div id="about" className="mt-10">
-        {/* <Reveal> */}
-        <span className="text-black dark:text-white my-5">
-          Hi, I&rsquo;m Kris
-          <span className="line-through text-red-500">h</span> Patel
-        </span>
 
-        {/* </Reveal> */}
-        <div className="text-black dark:text-white text-base ">
-          {/* <Reveal> */}
-          <p>
-            A 19-y/o computer engineering student from Ahmedabad, India . I love
+        <div className="w-full md:max-w-md text-base md:mt-1 mt-4">
+          <span className="text-black dark:text-white my-5">
+            Hi, I&rsquo;m Kris
+            <span className="line-through text-red-500">h</span> Patel ,
+          </span>
+
+          <p className="text-white ">
+            A 20-y/o computer engineering student from Ahmedabad, India. I love
             diving into the world of coding and finding creative solutions to
             challenges.
           </p>
+        </div>
+      </div>
+      {/* </Reveal> */}
+      <div id="about" className="mt-2">
+        {/* <Reveal> */}
+
+        {/* </Reveal> */}
+        <div className="text-white text-base ">
+          {/* <Reveal> */}
+
           {/* </Reveal> */}
           <hr className="my-5 opacity-30" />
           <div className="flex flex-col gap-3">
